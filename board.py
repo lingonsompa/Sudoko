5e6014204425e292ceb323cf69fe6878084db156
lingonsompa <60947241+lingonsompa@users.noreply.github.com>
2021-07-18 22:14:18 +0200

    Update board.py

--- board.py
+++ board.py
@@ -1,7 +1,9 @@
 import copy
+import math
+
 
 class Board:
-    def __init__(self, board=[[1,2,3],[4,5,6],[7,8,9]]):
+    def __init__(self, board=[[1,2,3,5],[5,6,7,8],[9,10,11,12],[13,14,15,0]]):
         self.board = board
     def __str__(self):
         row_str=''
@@ -28,13 +30,14 @@ class Board:
 
         for i in range(len(self.board)):
             t=copy.deepcopy(self)
-            t.board[x][y]=i
+            t.board[x][y]=i+1
             l.append(t)
         return l
 
     def check_x(self):
-        d={}
+
         for row in self.board:
+            row = [i for i in row if i != 0]
             if len(row) != len(set(row)):
                 return False
         return True
@@ -50,6 +53,7 @@ class Board:
 
 
         for l in ls:
+            l=[i for i in l if i != 0]
             if len(set(l))!=len(l):
                 return False
         return True
@@ -57,6 +61,35 @@ class Board:
 
     def check(self):
         return self.check_y() and self.check_x()
+
+    def is_filled(self):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if self.board[i][j] == 0:
+                    return False
+        else:
+            return True
+    def is_complete(self):
+        return self.check() and self.is_filled() and self.check_squares()
+
+    def check_squares(self):
+        length=int(math.sqrt(len(self.board)))
+        for j in range(length):
+            for k in range(length):
+                if (self.check_square(j*length,k*length,length))== False:
+                    return False
+        return True
+    def check_square(self, j,k,length):
+        l=[]
+        for o in range(length):
+            for p in range(length):
+                l.append(self.board[j+o][p+k])
+        l=l=[i for i in l if i != 0]
+        return len(set(l))==len(l)
+
+
+
+
 
 
 
@@ -70,4 +103,7 @@ class Board:
 
 if __name__ == '__main__':
     b=Board()
-    print(b.check())
+
+    for i in b.create_children():
+        print(i)
+        print()
